1fc6b07478698a449a6cbffe355af1938f225ab0
Tolorunju Oluwasegun <tolorunjuoluwasegunjeremiah@gmail.com>
2025-01-07 21:40:45 +0100

    Update and rename Greetings.py to Day2 Greetings.py

---
+++ Day2 Greetings.py
@@ -0,0 +1,6 @@
+# Program to greet user and calculate  birth year
+name = input("Enter your name:")
+age = int((input("Enter your age: ")))
+# calculating user birth year
+birth_year = 2025 - age
+print(f"hello, you are born in the year {birth_year}")

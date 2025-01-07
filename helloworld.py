136403465e163693e23ab9060c70db6983b16266
Tolorunju Oluwasegun <tolorunjuoluwasegunjeremiah@gmail.com>
2025-01-07 01:49:55 +0100

    Create helloworld.py
    day1

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+print("Hello world")
+# Printing Multiplication,subtration and division of Numbers.
+A = 5
+B = 7
+result = A * B
+print(result)
+result = A + B
+print(result)
+result = A - B
+print(result)
+result = A / B
+print(result)

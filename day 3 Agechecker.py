b744c36d537000d9e186c9557c1673670b8b931f
Tolorunju Oluwasegun <tolorunjuoluwasegunjeremiah@gmail.com>
2025-01-08 09:59:21 +0100

    Add files via upload

---
+++ day 3 Agechecker.py
@@ -0,0 +1,7 @@
+# asking for user age 
+age =int(input("Enter your age: "))
+# checking eligibility
+if age > 18: 
+    print("You are allowed to take some shot of Tequila")
+else:
+    print("You are not Qualified to take alcohol, As your age Limit do not permit" )

475df745ba320f4c610f0f1c62d43f0fa57c26cc
Tolorunju Oluwasegun <tolorunjuoluwasegunjeremiah@gmail.com>
2025-01-08 10:00:39 +0100

    Add files via upload

---
+++ Weight checker.py
@@ -0,0 +1,13 @@
+weight =int(input("Weight: "))
+unit = input("(K)g or (L)b: ")
+
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print("Weight in Lbs: " + str(converted))
+        
+else:
+    converted = weight * 0.45
+    print("Weight in Kgs: " + str(converted))
+
+            
+        

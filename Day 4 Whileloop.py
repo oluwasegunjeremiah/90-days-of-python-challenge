20dbc9336193bdec33879ddc396ad857e7afcf31
Tolorunju Oluwasegun <tolorunjuoluwasegunjeremiah@gmail.com>
2025-01-09 09:42:35 +0100

    Add files via upload

---
+++ Day 4 Whileloop.py
@@ -0,0 +1,10 @@
+# Printing For Loops
+nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+for num in nums:
+    print(num)
+
+# printing While loops
+number = 10
+while number > 0:
+    print(number)
+    number-=1

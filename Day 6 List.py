b8ea0b23b554cce951962e6f09fe46db83d1d259
Tolorunju Oluwasegun <tolorunjuoluwasegunjeremiah@gmail.com>
2025-01-11 23:35:52 +0100

    Add files via upload

---
+++ Day 6 List.py
@@ -0,0 +1,8 @@
+# Listing numbers
+numbers = [ 5, 10, 15, 20, 25, 7, 3, 12, 8, 3, 7,13]
+sum_of_numbers = sum(numbers)
+
+average = sum_of_numbers / len(numbers)
+  
+print("Sum:", sum_of_numbers)
+print("Average:", average)
